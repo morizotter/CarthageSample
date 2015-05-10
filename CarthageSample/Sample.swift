1e3c696e4b3fca2e976669a88e01b237625c880b
Naoki Morita <namorit@gmail.com>
2015-05-10 20:39:02 +0900

    Add

---
+++ CarthageSample/Sample.swift
@@ -0,0 +1,15 @@
+//
+//  Sample.swift
+//  CarthageSample
+//
+//  Created by MORITA NAOKI on 2015/05/10.
+//  Copyright (c) 2015年 molabo. All rights reserved.
+//
+
+import Foundation
+
+class Sample {
+    func log() -> String {
+        "sample"
+    }
+}
---
+++ CarthageSampleTests/CarthageSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  CarthageSampleTests.swift
+//  CarthageSampleTests
+//
+//  Created by MORITA NAOKI on 2015/05/10.
+//  Copyright (c) 2015年 molabo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CarthageSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
